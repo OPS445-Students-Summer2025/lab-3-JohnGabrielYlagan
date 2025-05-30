a93599470fcf4398c9d057a0824b524932de12d3
John Gabriel Ylagan <jgylagan@myseneca.ca>
2025-05-30 14:58:25 -0400

    lab3c.py with connditions

---
+++ lab3c.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+'''Lab 3 Function Receiving Multiple Arguments '''
+# Author ID: jgylagan
+
+'''
+def operate(number1, number2, operator):
+    # Place logic in this function
+
+if __name__ == '__main__':
+    print(operate(10, 5, 'add'))
+    print(operate(10, 5, 'subtract'))
+    print(operate(10, 5, 'multiply'))
+    print(operate(10, 5, 'divide'))
+'''
+
+def operate(number1, number2, operator): # operate function ready
+    if operator == 'add':
+        result = number1 + number2
+    elif operator == 'subtract':
+        result = number1 - number2
+    elif operator == 'multiply':
+        result = number1 * number2
+    else:
+        result = 'Error: function operator can be "add", "subtract", or "multiply"'
+    return result  # message to print if there is no 3rd argument or not typed correctly
+
+
+# Main Program
+
+if __name__ == '__main__':
+    print(operate(10, 5, 'add'))
+    print(operate(10, 5, 'subtract'))
+    print(operate(10, 5, 'multiply'))
+    print(operate(10, 5, 'divide'))
